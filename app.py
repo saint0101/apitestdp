832c8eaa6ea9b81a055b520d4003baedc2d7c475
fourier <fouriersaint@gmail.com>
2023-11-12 01:45:43 +0000

    test avec une api flask et une bd maridb

--- app.py
+++ app.py
@@ -1,58 +1,32 @@
-import uuid
-from flask import Flask, request
+# import the necessary packages
+import flask
+import json
+import mariadb
+from .db import config
 
-from .db import stores, items
-
-
-app = Flask(__name__)
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        return {"message": "Item not found"}, 404
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+# route to return all people
+@app.route('/api/people', methods=['GET'])
+def index():
+   # connection for MariaDB
+   conn = mariadb.connect(**config)
+   # create a connection cursor
+   cur = conn.cursor()
+   # execute a SQL statement
+   cur.execute("select * from people")
 
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
+   # serialize results into JSON
+   row_headers=[x[0] for x in cur.description]
+   rv = cur.fetchall()
+   json_data=[]
+   for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
 
-    return item
+   # return the results!
+   return json.dumps(json_data)
 
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        # Here you might also want to add the items in this store
-        # We'll do that later on in the course
-        return stores[store_id]
-    except KeyError:
-        return {"message": "Store not found"}, 404
-
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-
-    return store
-
-
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+# app.run()
+# app.run(debug=True, host='0.0.0.0')
--- db.py
+++ db.py
@@ -1,35 +1,45 @@
 # liste contenant les données
 
-stores = {}
-items = {}
+# stores = {}
+# items = {}
+#
+# _stores = {
+#     "stores": [
+#         {
+#             "items": [
+#                 {
+#                     "name": "my item",
+#                     "price": 15.99
+#                 }
+#             ],
+#             "name": "My Store"
+#         },
+#         {
+#             "items": [
+#                 {
+#                     "name": "tablette",
+#                     "price": 2000
+#                 },
+#                 {
+#                     "name": "tablette 2",
+#                     "price": 3000
+#                 },
+#                 {
+#                     "name": "tablette 2",
+#                     "price": 3000
+#                 }
+#             ],
+#             "name": "nouveau magasin 1"
+#         }
+#     ]
+# # }
 
-_stores = {
-    "stores": [
-        {
-            "items": [
-                {
-                    "name": "my item",
-                    "price": 15.99
-                }
-            ],
-            "name": "My Store"
-        },
-        {
-            "items": [
-                {
-                    "name": "tablette",
-                    "price": 2000
-                },
-                {
-                    "name": "tablette 2",
-                    "price": 3000
-                },
-                {
-                    "name": "tablette 2",
-                    "price": 3000
-                }
-            ],
-            "name": "nouveau magasin 1"
-        }
-    ]
+
+# configuration used to connect to MariaDB
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'password': 'Password123!',
+    'database': 'demo'
 }
